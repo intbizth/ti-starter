3799891d8744e9f5050fc4aa17d97c6063d59e35
liverbool <nukboon@gmail.com>
2016-06-13 03:34:02 +0700

    convert tab to space.

--- app/models/members.js
+++ app/models/members.js
@@ -1,14 +1,14 @@
 var BaseCollection = require('collection');
 
 exports.definition = BaseCollection.initCollection({
-	api: '/members/',
-	config: {
-		columns: {
-			"id": "integer",
-		    "fullname": "string"
-		},
-		adapter: {
-			collection_name: "members",
-		}
-	}
+    api: '/members/',
+    config: {
+        columns: {
+            "id": "integer",
+            "fullname": "string"
+        },
+        adapter: {
+            collection_name: "members",
+        }
+    }
 });
